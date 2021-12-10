12442cfdf94a94cc7179ad41273cad27d0cd2a8c
Sam Glover <sam@samglover.net>
2021-12-10 09:44:47 -0600

    Remove hardcoded XML sitemap link.

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
   <ul id="footer-widgets"><?php dynamic_sidebar( 'footer-widgets' ); ?></ul>
 
-  <p class="copyright"><small>The original content within this website is &copy; <?php echo date('Y') ?>. <a href="https://samglover.net/sitemap_index.xml">XML Sitemap</a></small></p>
+  <p class="copyright"><small>The original content within this website is &copy; <?php echo date('Y') ?>.</small></p>
 
 </footer>
 
