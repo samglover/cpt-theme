b3cc314019691bd36b769cebefb91b48d020482d
samglover <sam@samglover.net>
2022-03-17 23:50:50 -0500

    Minor tweaks.

--- template-parts/loop-page.php
+++ template-parts/loop-page.php
@@ -5,16 +5,12 @@
     <?php if ( ! is_front_page() ) { ?>
       <h1 class="headline"><?php the_title(); ?></h1>
     <?php } ?>
-
     <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-
     <?php the_content(); ?>
     <div class="clearfix"></div>
-
     <?php if ( ! is_front_page() ) { ?>
       <p class="post-byline">By <?php the_author(); ?>. Last updated on <?php the_modified_date( 'F jS, Y' ); ?>.</p>
     <?php } ?>
-
     <?php wp_link_pages(); ?>
   </main>
 <?php endwhile; endif; ?>
--- template-parts/loop-single.php
+++ template-parts/loop-single.php
@@ -4,12 +4,9 @@
   <main id="content" <?php post_class(); ?>>
     <h1 class="post-title"><?php the_title(); ?></h1>
     <p class="post-byline">By <?php the_author(); ?> on <?php the_date( 'F jS, Y' ); ?></p>
-
     <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-
     <?php the_content(); ?>
     <div class="clearfix"></div>
-
     <?php wp_link_pages(); ?>
   </main>
 <?php endwhile; endif; ?>
