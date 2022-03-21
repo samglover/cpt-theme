f6c3fccc4f3fdd3734a6501c334b797feff57255
Sam Glover <sam@samglover.net>
2022-03-21 16:45:33 -0500

    Saving WIP, still not working.

--- assets/js/menus.js
+++ assets/js/menus.js
@@ -4,62 +4,43 @@
  * If the menu overflows the width of the menu container, this function
  * collapses the overflow into a new sub-menu.
  */
-const Padding = 20;
-const Menu    = document.querySelector('#primary-menu > ul.menu');
+const Menu = document.querySelector('#primary-menu > ul.menu')
 
-const CollapsedMenu = document.createElement( 'li' );
-      CollapsedMenu.classList.add( 'menu-item', 'menu-item-has-children', 'collapsed-menu' );
-      CollapsedMenu.insertAdjacentHTML( 'afterbegin', '<i>&bull;&bull;&bull;</i>' );
-
-const CollapsedSubMenu = document.createElement( 'ul' );
-      CollapsedSubMenu.classList.add( 'sub-menu' );
-
-CollapsedMenu.appendChild( CollapsedSubMenu );
+// Creates the collapsed menu item.
+const CollapsedMenu = document.createElement('li');
+      CollapsedMenu.classList.add('menu-item', 'menu-item-has-children', 'collapsed-menu');
+      CollapsedMenu.insertAdjacentHTML('afterbegin', '<i>&bull;&bull;&bull;</i>');
+const CollapsedSubMenu = document.createElement('ul');
+      CollapsedSubMenu.classList.add('sub-menu');
+CollapsedMenu.appendChild(CollapsedSubMenu);
 
 function primaryMenuCollapser() {
   if ( ! Menu ) { return; }
-  let stackedHeader = false;
-
-  if ( getSpaceAvailable() > getSpaceNeeded() ) {
-    resetMenu();
-  }
-
-  if ( getSpaceAvailable() <= getSpaceNeeded() ) {
-    collapseMenu();
-  }
+  if ( ! Menu.contains(CollapsedMenu) && getSpaceAvailable() > getSpaceNeeded() ) { resetMenu(); }
+  if ( getSpaceAvailable() <= getSpaceNeeded() ) { collapseMenu(); }
 
   function getSpaceAvailable() {
     return document.querySelector('#primary-menu').offsetWidth;
   }
 
   // Calculates the space needed for the menu.
-  // The + 1 is a cheap "round-up" in case of fractional pixels.
   function getSpaceNeeded() {
-    if ( CollapsedSubMenu.length > 0 ) {
-      let collapsedMenuWidth = document.querySelector('.collapsed-menu > i').scrollWidth;
-      let nextItem      = CollapsedSubMenu.children[0].children[0];     // Gets the <a> element inside the <li> element.
-      let nextItemWidth = nextItem ? (nextItem.scrollWidth * 1.25) : 0; // Multiplied by 1.25 to account for the different font sizes.
-
-      if ( nextItem && nextItem.length == 1 ) {
-        return Menu.scrollWidth - collapsedMenuWidth + nextItemWidth + 1;
-      } else {
-        return Menu.scrollWidth + nextItemWidth + 1;
-      }
-    } else {
-      return Menu.scrollWidth;
-    }
+    let nextItemWidth = CollapsedSubMenu.querySelector('li:first-of-type > a') ? CollapsedSubMenu.querySelector('li:first-of-type > a').scrollWidth : 0;
+    console.log(nextItemWidth);
+    return Menu.scrollWidth + nextItemWidth + 1;
   }
 
   function resetMenu() {
     let primaryMenuItems    = Menu.querySelectorAll('.menu-item:not(.sub-menu .menu-item):not(.collapsed-menu)');
     let collapsedMenuItems  = CollapsedSubMenu.querySelectorAll('.menu-item:not(.sub-menu .sub-menu .menu-item)');
-
     collapsedMenuItems.forEach( function(e) {
       Menu.appendChild(e);
     });
-
     if ( Menu.contains(CollapsedMenu) ) {
       Menu.removeChild(CollapsedMenu);
+    }
+    if ( getSpaceAvailable() <= getSpaceNeeded() ) {
+      collapseMenu();
     }
   }
 
@@ -69,10 +50,8 @@ function primaryMenuCollapser() {
     if ( ! Menu.contains(CollapsedMenu) ) {
       Menu.appendChild(CollapsedMenu);
     }
-
     let primaryMenuItems    = Menu.querySelectorAll('.menu-item:not(.sub-menu .menu-item):not(.collapsed-menu)');
     let collapsedMenuItems  = CollapsedSubMenu.querySelectorAll('.menu-item:not(.sub-menu .sub-menu .menu-item)');
-
     primaryMenuItems.forEach( function(e) {
       if ( getSpaceAvailable() <= getSpaceNeeded() ) {
         let lastItem = primaryMenuItems.length - 1;
@@ -83,5 +62,6 @@ function primaryMenuCollapser() {
     });
   }
 }
-primaryMenuCollapser();
+
+window.onload = primaryMenuCollapser;
 window.onresize = primaryMenuCollapser;
