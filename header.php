af4140563e47acf35c91e840b099e90e39db0a54
Sam Glover <sam@samglover.net>
2022-07-27 14:03:43 -0500

    Update header.php

--- header.php
+++ header.php
@@ -3,16 +3,16 @@
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
   <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'cpt-theme'); ?></a>
-  <?php get_template_part('template-parts/site-header'); ?>
 
   <div id="page-content" class="site">
+    <?php get_template_part('template-parts/site-header'); ?>
     <div id="content" class="site-content">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main">
