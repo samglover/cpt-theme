8cfba7906d4b2b904bf61aae627a497f4238f839
samglover <sam@samglover.net>
2022-11-27 17:05:49 -0600

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
     <div id="content">
       <h1>404 Not Found</h1>
       <p>Sorry, I know this isn't what you were hoping to see. The page you were looking for is probably gone, but you can try searching for it just to make sure:</p>
-      <form role="search" method="get" action="http://robots.local/" class="wp-block-search__button-outside wp-block-search__text-button wp-block-search">
+      <form role="search" method="get" action="<?php echo esc_url(home_url()); ?>" class="wp-block-search__button-outside wp-block-search__text-button wp-block-search">
         <label for="wp-block-search__input-1" class="wp-block-search__label screen-reader-text">Search</label>
         <div class="wp-block-search__inside-wrapper ">
           <input type="search" id="wp-block-search__input-1" class="wp-block-search__input " name="s" value="<?php echo esc_attr(get_search_query()); ?>" placeholder="" required="">
