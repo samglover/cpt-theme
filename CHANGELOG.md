0e2663702607563f14853711579c96f6bece620f
samglover <sam@samglover.net>
2024-01-17 18:52:15 -0600

    3.0.9beta1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,10 +3,11 @@
 All notable changes to this project will be documented in this file. The format
 is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
-## 3.0.9
+## 3.0.9beta1
 
 ### Changed
 - Name updated to Client Power Tools Theme since there are also several Client Power Tools plugins.
+- Simplify modal show/hide logic.
 
 ### Added
 - Customizations for the Client Power Tools Portal plugin if it is installed and active.
--- assets/js/cta-modal.js
+++ assets/js/cta-modal.js
@@ -3,37 +3,23 @@ wp.domReady(() => {
   if (!ctaModal) return;
   
   const ctaButton = document.getElementById('header-cta');
+  const ctaDismiss = document.querySelector('#cta-modal .cta-modal-dismiss');
   const ctaLinks = document.querySelectorAll('a[href*="#cpt-cta"]');
-  const ctaDismiss = ctaModal.querySelector('#cta-modal .modal-dismiss');
-  const ctaModalScreen = document.getElementById('cta-modal-screen');
   
-  if (ctaButton) ctaButton.addEventListener('click', toggleModal);
-  if (ctaLinks) ctaLinks.forEach(element => element.addEventListener('click', toggleModal));
-  
-    addEventListener('keyup', (event) => {
-      if (event.key === 'Escape') closeModal();
-    });
-    document.addEventListener('click', checkClick);
-  
-  function toggleModal(event) {
-    event.preventDefault();
-    if (ctaModal.classList.contains('visible')) {
-      ctaModal.classList.remove('visible');
-    } else {
-      ctaModal.classList.add('visible');
-    }
+  if (ctaButton) ctaButton.addEventListener('click', showModal);
+  if (ctaDismiss) ctaDismiss.addEventListener('click', closeModal);
+  if (ctaLinks) ctaLinks.forEach(element => element.addEventListener('click', showModal));
+  addEventListener('keyup', (event) => {
+    if (event.key === 'Escape') closeModal();
+  });
+
+  function showModal(event) {
+    if (event) event.preventDefault();
+    ctaModal.classList.add('visible');
   }
-  
-  function checkClick(event) {
-    if (
-      !event.target.closest('#header-cta') || 
-      event.target.classList.contains('modal-dismiss')
-    ) {
-      closeModal();
-    }
-  }
-  
-  function closeModal() {
+
+  function closeModal(event) {
+    if (event) event.preventDefault();
     ctaModal.classList.remove('visible');
   }
 });
